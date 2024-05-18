46687ed434631b8002733dc3694d231adbf89ddf
ArdiHanafi <ardihanafi5@gmail.com>
2024-05-18 14:39:59 +0700

    feat: :wrench: Add Arabic language

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -2,7 +2,7 @@ import { Config } from "next-i18n-router/dist/types";
 
 const i18nConfig: Config = {
     defaultLocale: 'en',
-    locales: ['en', 'id', 'fr', 'it'],
+    locales: ['en', 'id', 'fr', 'it', 'ar'],
 };
 
 export default i18nConfig;
